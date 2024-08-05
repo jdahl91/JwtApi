7d7d80036db7c6d79907fccae929368353ff66f6
jdahl91 <102322560+jdahl91@users.noreply.github.com>
2024-08-05 16:34:47 -0700

    Update JwtApiDeployScript.ps1
    removed personal information

--- JwtApiDeployScript.ps1
+++ JwtApiDeployScript.ps1
@@ -3,6 +3,6 @@
 Write-Output "Starting publish script."
 cd C:\Users\joakim\source\repos\jdahl91\JwtApi\bin\Release\net8.0\publish
 
-scp -r * aspnetcore@172.233.145.243:/home/aspnetcore/JwtApi
+scp -r * xyz@xxx.xxx.xxx.xxx:/home/xyz/JwtApi
 
 Write-Output "App successfully published to remote server."
